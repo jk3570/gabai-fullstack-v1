c569566e69fd35f0423301b9ac25429c20d25ff6
sirLeo <114740132+nardLeano@users.noreply.github.com>
2024-03-05 01:01:03 +0800

    update navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuthContext } from "./hooks/useAuthContext";
+import { useState, useEffect } from 'react';
 
 //import { useAuthContext } from './hooks/useAuthContext'
 
@@ -52,30 +53,56 @@ function Header() {
     });
   };
 
+  const [navbarHeight, setNavbarHeight] = useState('py-10'); // Initial navbar padding
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Check the scroll position
+      if (window.scrollY > 50 ) {
+        // If scrolled down, update navbar padding
+        setNavbarHeight('py-5');
+      } else {
+        // If scrolled to the top, revert to original navbar padding
+        setNavbarHeight('py-10');
+      }
+    };
+
+    // Add scroll event listener when component mounts
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener when component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []); 
+
+
   return (
     <>
       {/* Navigation Bar  */}
-      <nav className="sticky top-0 z-50 bg-white h-[3.875rem] w-full px-[1.25rem] font-bold border-b-2 border-azure-500">
+      <nav className={`sticky top-0 z-50 bg-white h-[3.875rem] w-full font-bold border-b-2 border-azure-500 transition-all duration-300 ${navbarHeight}`}>
         {/* Brand Name */}
-        <Link to="/">
-          <div onClick={scrollToTop} className="">
-            <img src={Logo} alt="Logo" style={{ height: "25px" }} />
+          <div className="w-full max-w-4xl flex justify-between items-center">
+            <Link to="/" >
+              <div onClick={scrollToTop} className="">
+                <img src={Logo} alt="Logo" style={{ height: "25px" }} />
+              </div>
+            </Link>
+
+            {/* Search Bar */}
+            <div className="flex flex-row items-center text-md gap-x-5">
+              {/* Login Btn */}
+              <Login />
+
+              {/* Search Icon */}
+              <Link to="/search">
+                <FaSearch className="text-2xl" />
+              </Link>
+
+              {/* Toggle night mode */}
+              <BsMoon className="text-2xl" onClick={toggle} />
+            </div>
           </div>
-        </Link>
-
-        {/* Search Bar */}
-        <div className="flex flex-row items-center text-md gap-x-5">
-          {/* Login Btn */}
-          <Login />
-
-          {/* Search Icon */}
-          <Link to="/search">
-            <FaSearch className="text-2xl" />
-          </Link>
-
-          {/* Toggle night mode */}
-          <BsMoon className="text-2xl" onClick={toggle} />
-        </div>
       </nav>
     </>
   );
