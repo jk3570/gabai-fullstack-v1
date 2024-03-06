342295e6771bb2fe6b6cad205b429bfbd85a7b5d
leiocopio <107382632+leiocopio@users.noreply.github.com>
2024-03-06 22:55:42 +0800

    Update chat.js

--- frontend/src/pages/chat/chat.js
+++ frontend/src/pages/chat/chat.js
@@ -1,25 +1,57 @@
 import { BsArrowRight } from "react-icons/bs";
 
-function chat() {
+function Chat() {
+  const username = "User";
+  const textbox = "w-full h-10 p-2 rounded-full border-2 border-gray-500";
+  const submitBtn = "bg-azure text-white font-bold py-2 px-4 rounded-md";
+
+  const prompt = [
+    {
+      message:
+        "Hi Gab, I've been facing some issues at work. I think I'm experiencing discrimination. Can you help?",
+      name: username,
+      color: "bg-gray-500",
+    },
+    {
+      message:
+        "I'm here for you. I'm sorry to hear about your situation. Can you provide some details about the discrimination you're facing?",
+      name: "Gab",
+      color: "bg-azure",
+    },
+  ];
+
+  // Reverse the prompt array to display messages from bottom to top
+
   return (
-    <div className="chat h-screen">
-      <div id="chat-container">
-        <form action="">
-          <input
-            type="text"
-            name=""
-            id=""
-            className="p-2 border-2 border-azure-500 rounded-md"
-            placeholder="Type a message..."
-          />
-          <button type="submit" className="p-2 bg-azure-500 rounded-md">
-            Send
-            <BsArrowRight className="text-white" />
-          </button>
-        </form>
+    <div className="w-full h-[60vh] relative max-w-4xl px-5 lg:px-0 mx-auto mt-20">
+      <div className="chatbox mb-12 bottom-0">
+        {prompt.map((item, index) => (
+          <div key={index} className="p-5 bg-gray-100 ">
+            <div className="flex flex-row gap-3 items-center">
+              <div
+                className={
+                  item.color +
+                  " h-10 w-10 rounded-full flex justify-center items-center"
+                }
+              ></div>
+              <p>
+                <b>{item.name}</b>
+              </p>
+            </div>
+            <br />
+            <p className="ml-10">{item.message}</p>
+          </div>
+        ))}
       </div>
+
+      <form action="" className="flex flex-row gap-1 absolute bottom-0 w-full">
+        <input type="text" name="" id="" className={textbox} />
+        <button type="submit" className={submitBtn}>
+          Send
+        </button>
+      </form>
     </div>
   );
 }
 
-export default chat;
+export default Chat;
